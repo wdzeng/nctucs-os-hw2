004c2f8f1bd7b22cc5c783f1302dc14f28393147
hyperbola <hyperbola.cs07@gmail.com>
2021-01-14 23:10:13 +0800

    123

---
+++ 0716023/server.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#define SHM_KEY 716023
+#define SHM_FILE "/tmp/i_love_aries"
+
+int main() {
+    int id;
+
+    /************************* TODO 1 *************************/
+    // Create a shared memory section
+    int shmid = shmget(SHM_KEY, sizeof(int), 0666 | IPC_CREAT | IPC_EXCL);
+    if (shmid < 0) {
+        perror("Failed to create shared memory");
+        exit(-1);
+    }
+
+    /************************* TODO 1 *************************/
+
+    /************************* TODO 2 *************************/
+    // Attach the memory section
+    // the return value is a pointer to the shared memory section
+    int *ptr = (int *)shmat(shmid, (void *)0, 0);
+    /************************* TODO 2 *************************/
+
+    ptr[0] = 0;
+    printf("\033[1;32m[server] The value is %d\033[0m\n", ptr[0]);
+
+    while (1) {
+        int cmd;
+
+        printf("\n");
+        printf("1: Show the value\n");
+        printf("2: Modify the value\n");
+        printf("3: Exit\n");
+        printf("Enter commands: ");
+        scanf("%d", &cmd);
+
+        if (cmd == 1)
+            printf("\033[1;32m[server] The value is %d\033[0m\n", ptr[0]);
+        else if (cmd == 2) {
+            printf("Input new value: ");
+            scanf("%d", &ptr[0]);
+        } else
+            break;
+    }
+    // detach from shared memory
+    shmdt(ptr);
+
+    // destroy the shared memory
+    shmctl(shmid, IPC_RMID, NULL);
+}
--- page_table.c
+++ page_table.c
@@ -1,95 +1,143 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
+
 #include "page_table_driver.h"
 
 int fd;
 
 // Obtain my cr3 value (a.k.a. PML4 table physical address)
-uint64_t get_cr3_value()
-{
-	struct ioctl_arg cmd;
-	int ret;
-	cmd.request[0] = IO_CR3;
-	ret = ioctl(fd, IOCTL_REQUEST, &cmd);
-	return cmd.ret;
+uint64_t get_cr3_value() {
+    struct ioctl_arg cmd;
+    int ret;
+    cmd.request[0] = IO_CR3;
+    ret = ioctl(fd, IOCTL_REQUEST, &cmd);
+    return cmd.ret;
 }
 
 // Given a physical address, return the value
-uint64_t read_physical_address(uint64_t physical_address)
-{
-	struct ioctl_arg cmd;
-	int ret;
-	cmd.request[0] = IO_READ;
-	cmd.request[1] = physical_address;
-	ret = ioctl(fd, IOCTL_REQUEST, &cmd);
-	return cmd.ret;
+uint64_t read_physical_address(uint64_t physical_address) {
+    struct ioctl_arg cmd;
+    int ret;
+    cmd.request[0] = IO_READ;
+    cmd.request[1] = physical_address;
+    ret = ioctl(fd, IOCTL_REQUEST, &cmd);
+    return cmd.ret;
 }
 
 // Write value to a physical address
-void write_physical_address(uint64_t physical_address, uint64_t value)
-{
-	struct ioctl_arg cmd;
-	int ret;
-	cmd.request[0] = IO_WRITE;
-	cmd.request[1] = physical_address;
-	cmd.request[2] = value;
-	ret = ioctl(fd, IOCTL_REQUEST, &cmd);
+void write_physical_address(uint64_t physical_address, uint64_t value) {
+    struct ioctl_arg cmd;
+    int ret;
+    cmd.request[0] = IO_WRITE;
+    cmd.request[1] = physical_address;
+    cmd.request[2] = value;
+    ret = ioctl(fd, IOCTL_REQUEST, &cmd);
 }
 
-int main()
-{
-	char *x = (char*)aligned_alloc(4096, 4096) + 0x123;
-	char *y = (char*)aligned_alloc(4096, 4096) + 0x123;
-	strcpy(x, "This is a simple HW.");
-	strcpy(y, "You have to modify my page table.");
+uint64_t cut(uint64_t v, int h, int l) {
+    v >>= l;
+    uint64_t mask = 1;
+    mask <<= (h - l + 1);
+    mask--;
+    return v & mask;
+}
 
-	fd = open("/dev/os", O_RDONLY);
-	if(fd < 0) 
-	{
-		printf("Cannot open device!\n");
-		return 0;
-	}
+uint64_t addrpml4(uint64_t a) {
+    a = cut(a, 47, 39);
+    a <<= 3;
+    uint64_t cr3 = get_cr3_value();
+    cr3 >>= 12;
+    cr3 <<= 12;
+    cr3 |= (a & 0xfff);
+    return cr3;
+}
 
-	printf("Before\n");
-	printf("x : %s\n", x);
-	printf("y : %s\n", y);
+uint64_t addrpdpt(uint64_t a) {
+    uint64_t pml4 = read_physical_address(addrpml4(a));
+    pml4 >>= 12;
+    pml4 <<= 12;
+    a = cut(a, 38, 30);
+    a <<= 3;
+    return pml4 | (a & 0xfff);
+}
 
-	/* TODO 1 */
-	// ------------------------------------------------
-	// Modify page table entry of y
-	// Let y point to x's physical address
-	// ------------------------------------------------
+uint64_t addrpd(uint64_t a) {
+    uint64_t pdpt = read_physical_address(addrpdpt(a));
+    pdpt >>= 12;
+    pdpt <<= 12;
+    a = cut(a, 29, 21);
+    a <<= 3;
+    return pdpt | (a & 0xfff);
+}
 
-	getchar();
+uint64_t addrpt(uint64_t a) {
+    uint64_t pd = read_physical_address(addrpd(a));
+    pd >>= 12;
+    pd <<= 12;
+    a = cut(a, 20, 12);
+    a <<= 3;
+    return pd | (a & 0xfff);
+}
 
-	printf("After modifying page table\n");
-	printf("x : %s\n", x);
-	printf("y : %s\n", y);
+int main() {
+    char *x = (char *)aligned_alloc(4096, 4096) + 0x123;
+    char *y = (char *)aligned_alloc(4096, 4096) + 0x123;
+    strcpy(x, "This is a simple HW.");
+    strcpy(y, "You have to modify my page table.");
 
-	getchar();
+    fd = open("/dev/os", O_RDONLY);
+    if (fd < 0) {
+        printf("Cannot open device!\n");
+        return 0;
+    }
 
-	strcpy(y, "When you modify y, x is modified actually.");
-	printf("After modifying string y\n");
-	printf("x : %s\n", x);
-	printf("y : %s\n", y);
+    printf("Before\n");
+    printf("x : %s\n", x);
+    printf("y : %s\n", y);
 
-	/* TODO 2 */
-	// ------------------------------------------------
-	// Recover page table entry of y
-	// Let y point to its original address
-	// You may need to store y's original address at previous step
-	// ------------------------------------------------
+    /* TODO 1 */
+    // ------------------------------------------------
+    // Modify page table entry of y
+    uint64_t yAddrPt = addrpt((uint64_t)y);
+	uint64_t oldYValPt = read_physical_address(yAddrPt);
+    uint64_t xAddrPt = addrpt((uint64_t)x);
+	uint64_t xValPt = read_physical_address(xAddrPt);
+	write_physical_address(yAddrPt, xValPt);
 
-	getchar();
+    // Let y point to x's physical address
+    // ------------------------------------------------
 
-	printf("After recovering page table of y\n");
-	printf("x : %s\n", x);
-	printf("y : %s\n", y);
+    getchar();
 
-	close(fd);
+    printf("After modifying page table\n");
+    printf("x : %s\n", x);
+    printf("y : %s\n", y);
+
+    getchar();
+
+    strcpy(y, "When you modify y, x is modified actually.");
+    printf("After modifying string y\n");
+    printf("x : %s\n", x);
+    printf("y : %s\n", y);
+
+    /* TODO 2 */
+    // ------------------------------------------------
+    // Recover page table entry of y
+    // Let y point to its original address
+    // You may need to store y's original address at previous step
+	write_physical_address(yAddrPt, oldYValPt);
+    // ------------------------------------------------
+
+    getchar();
+
+    printf("After recovering page table of y\n");
+    printf("x : %s\n", x);
+    printf("y : %s\n", y);
+
+    close(fd);
 }
